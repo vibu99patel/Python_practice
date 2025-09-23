76345942538a2cc3002b545f9058fcae52cdb5d5
vaibhavi <vaibhavi99patel@gmail.com>
2025-09-23 10:53:17 -0500

    unpacking

--- test.py
+++
@@ -1,2 +0,0 @@
-number = (1, 2, 3)
-print(number)
--- tuple.py
+++ tuple.py
@@ -1,5 +1,18 @@
 numbers = (1, 2, 3)
-print(numbers[0])
+print(numbers[1])
 
-# can't change or add to tuple
-# you can only count the ocuurances
+# can't change or add to a tuple
+# you can only use:
+# count() the occurrences
+# and index() to find index of the item occurrences
+
+# unpacking
+coordinates = (1, 2, 3)
+# 1 way
+x = coordinates[0]
+y = coordinates[1]
+z = coordinates[2]
+print(x)
+# other way
+x, y, z = coordinates
+print(x)
