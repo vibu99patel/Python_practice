7a5aede1cab4561f071338de918bc80483fbe0ba
vaibhavi <vaibhavi99patel@gmail.com>
2025-09-23 11:30:08 -0500

    func with return statement

--- functions.py
+++ functions.py
@@ -17,3 +17,15 @@ greet_user("John Smith")
 print("Finish")
 
 # positional arguement comes first then keyword arguments
+print('\n')
+# return statement - returns value to functions
+def square(num):
+    return num*num
+
+def cube(num):
+    return num*num*num
+
+res = square(3)
+print(res)
+
+print(cube(3))
