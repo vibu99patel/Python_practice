0503ddd9c86aa95bafe2c61797f2a55ae4abd690
vaibhavi <vaibhavi99patel@gmail.com>
2025-09-24 17:50:22 -0500

    how module works

---
+++ converters.py
@@ -0,0 +1,6 @@
+def lb_to_kg(weight):
+    return weight*0.45
+
+
+def kg_to_lb(weight):
+    return weight/0.45
---
+++ modules.py
@@ -0,0 +1,7 @@
+import converters
+from converters import kg_to_lb
+
+# when you use line 1
+print(converters.lb_to_kg(70))
+# when you use line 2
+print(kg_to_lb(101))
